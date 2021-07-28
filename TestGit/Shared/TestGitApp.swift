9a97994bf23b268f9b5156893cc45091bda5af7a
wandong <wandd_1993@163.com>
2021-07-28 21:48:31 +0800

    wandong add 20210729

---
+++ TestGit/Shared/TestGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitApp.swift
+//  Shared
+//
+//  Created by 万冬冬 on 2021/7/28.
+//
+
+import SwiftUI
+
+@main
+struct TestGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
